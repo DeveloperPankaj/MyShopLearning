1afd037bc8d6a07ad233d76ed682cba412cad723
DeveloperPankaj <42271629+DeveloperPankaj@users.noreply.github.com>
2019-10-29 10:58:12 +0530

    Product Grouping Adding

--- MyShopLearning/MyShopLearning.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShopLearning/MyShopLearning.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -19,7 +19,7 @@ namespace MyShopLearning.WebUI.Tests.Controllers
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = controller.Index() as ViewResult;// .Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
