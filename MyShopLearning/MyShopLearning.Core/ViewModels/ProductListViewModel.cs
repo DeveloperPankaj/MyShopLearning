5712e31804954ee3a5397db78c04954de58ddd81
DeveloperPankaj <42271629+DeveloperPankaj@users.noreply.github.com>
2019-11-07 06:10:52 +0530

    Created Product Filter

---
+++ MyShopLearning/MyShopLearning.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyShopLearning.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopLearning.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- MyShopLearning/MyShopLearning.WebUI/Controllers/HomeController.cs
+++ MyShopLearning/MyShopLearning.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShopLearning.Core.Contracts;
 using MyShopLearning.Core.Models;
+using MyShopLearning.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,23 @@ namespace MyShopLearning.WebUI.Controllers
             context = productContext;//new InMemoryRepository<Product>();
             productCategories = productCategoryContext;//new InMemoryRepository<ProductCategory>();
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category = null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+            if(Category ==null)
+            {
+               products   = context.Collection().ToList();
+            }else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            return View(model);
         }
 
         public ActionResult Details(string Id)
