b5834048bccdb862d238e9ef587ddbcb42596828
DeveloperPankaj <42271629+DeveloperPankaj@users.noreply.github.com>
2019-11-10 18:02:39 +0530

    Creating BasketUI

--- MyShopLearning/MyShopLearning.WebUI/App_Start/UnityConfig.cs
+++ MyShopLearning/MyShopLearning.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using MyShopLearning.Core.Contracts;
 using MyShopLearning.Core.Models;
 using MyShopLearning.DataAccess.InMemory;
 using MyShopLearning.DataAccess.SQL;
+using MyShopLearning.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace MyShopLearning.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>,SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
